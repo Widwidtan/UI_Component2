43b14c9c8fb6166bf7d31cedbaba2767ec5e4b70
Widya Hidayatul <112817093+Widwidtan@users.noreply.github.com>
2022-10-11 09:51:21 +0700

    Initial commit

---
+++ Assets/Scenes/Volume.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Volume : MonoBehaviour
+{
+    [SerializeField] Slider VolumeSlider;
+
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+
+        else
+        {
+            Load();
+        }
+    }
+
+    public void ChangeVolume()
+    {
+      AudioListener.volume = VolumeSlider.value;  
+    }
+
+    private void Load()
+    {
+        VolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", VolumeSlider.value);
+    }
+}
